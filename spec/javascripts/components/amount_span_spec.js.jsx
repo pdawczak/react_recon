9e4587b8945f2c2cadf8b9929fd6a277fe30d1cc
Pawel Dawczak <pawel.dawczak@gmail.com>
2015-11-17 16:41:20 +0000

    Add specs for amount component

--- spec/javascripts/components/amount_span_spec.js.jsx
+++ spec/javascripts/components/amount_span_spec.js.jsx
@@ -8,6 +8,26 @@ describe("AmountSpan", function () {
 
     var spanNode = ReactDOM.findDOMNode(span);
 
-    expect(spanNode.textContent).toBe("£ 99.90");
+    expect(spanNode.textContent).toBe("£ 99.99");
+  });
+
+  it("formats the amount with two decimal places", function () {
+    var span = TestUtils.renderIntoDocument(
+      <AmountSpan amount={20} />
+    );
+
+    var spanNode = ReactDOM.findDOMNode(span);
+
+    expect(spanNode.textContent).toBe("£ 20.00");
+  });
+
+  it("renders 0.00 as value if none provided", function () {
+    var span = TestUtils.renderIntoDocument(
+      <AmountSpan />
+    );
+
+    var spanNode = ReactDOM.findDOMNode(span);
+
+    expect(spanNode.textContent).toBe("£ 0.00");
   });
 });
